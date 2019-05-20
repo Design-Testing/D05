0e7abc6b7f805720d4d84dcffbfd136a02e89b78
Jesús Manuel García Lanzas <jesgarlan@alum.us.es>
2019-05-20 13:49:11 +0200

    Lesson y Teacher (Rep&Serv)

--- Item 3/Educafy/src/main/java/services/LessonService.java
+++ Item 3/Educafy/src/main/java/services/LessonService.java
@@ -1,8 +1,10 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,18 +12,116 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.LessonRepository;
+import domain.Lesson;
+import domain.Reservation;
+import domain.Teacher;
 
 @Service
 @Transactional
 public class LessonService {
 
 	@Autowired
-	LessonRepository	lessonRepository;
+	private LessonRepository	lessonRepository;
 
+	@Autowired
+	private TeacherService		teacherService;
+
+	@Autowired
+	private ReservationService	reservationService;
+
+
+	public Lesson create() {
+		final Lesson lesson = new Lesson();
+		final Teacher principal = this.teacherService.findByPrincipal();
+		lesson.setTeacher(principal);
+		final String ticker = this.generateTicker(principal.getName());
+		lesson.setTicker(ticker);
+		lesson.setDraft(true);
+
+		return lesson;
+	}
+
+	public Lesson findOne(final int lessonId) {
+		Assert.isTrue(lessonId != 0);
+		final Lesson result = this.lessonRepository.findOne(lessonId);
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Collection<Lesson> findAll() {
+		Collection<Lesson> res = new ArrayList<>();
+		res = this.lessonRepository.findAll();
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Lesson save(final Lesson lesson) {
+		Assert.notNull(lesson);
+		final Teacher principal = this.teacherService.findByPrincipal();
+		final Lesson result;
+
+		if (lesson.getId() != 0) {
+			Assert.isTrue(lesson.getTeacher().equals(principal));
+			Assert.isTrue(lesson.isDraft(), "No puede modificar una posición que ya no esta en DRAFT MODE.");
+		}
+		result = this.lessonRepository.save(lesson);
+		return result;
+
+	}
+
+	public void delete(final Lesson lesson) {
+		Assert.notNull(lesson);
+		Assert.isTrue(lesson.getId() != 0);
+		final Lesson retrieved = this.findOne(lesson.getId());
+		final Teacher principal = this.teacherService.findByPrincipal();
+		Assert.isTrue(retrieved.getTeacher().equals(principal));
+		final List<Reservation> reservations = (List<Reservation>) this.reservationService.findAllReservationByLesson(lesson.getId());
+		Assert.isTrue(reservations.isEmpty(), "No puede borrar una lesson que tenga reservations.");
+		this.lessonRepository.delete(lesson);
+	}
+
+	/* ========================= OTHER METHODS =========================== */
+
+	public Collection<Lesson> findAllByPrincipal() {
+		Collection<Lesson> res = new ArrayList<>();
+		final Teacher principal = this.teacherService.findByPrincipal();
+		res = this.lessonRepository.findAllLessonByTeacherId(principal.getUserAccount().getId());
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Lesson toFinalMode(final int lessonId) {
+		final Lesson lesson = this.findOne(lessonId);
+		Assert.notNull(lesson);
+		final Teacher teacher = this.teacherService.findByPrincipal();
+		final Lesson result;
+		Assert.isTrue(lesson.getTeacher().equals(teacher), "No puede ejecutar ninguna acción sobre una lesson que no le pertenece.");
+		Assert.isTrue(lesson.isDraft(), "Para poner una position en FINAL MODE debe de estar anteriormente en DRAFT MODE.");
+		lesson.setDraft(false);
+		result = this.lessonRepository.save(lesson);
+		return result;
+	}
 
 	public Collection<Lesson> findPositions(final String keyword, final Double minSalary, final Double maxSalary, final Date minDeadline, final Date maxDeadline) {
 		final Collection<Lesson> res = this.lessonRepository.findLessons(keyword, minSalary, maxSalary, minDeadline, maxDeadline);
 		Assert.notNull(res);
+		return res;
+	}
+
+	//TODO: Revisar ticker
+	private String generateTicker(final String companyName) {
+		String res = "";
+		final Integer n1 = (int) Math.floor(Math.random() * 9 + 1);
+		final Integer n2 = (int) Math.floor(Math.random() * 9 + 1);
+		final Integer n3 = (int) Math.floor(Math.random() * 9 + 1);
+		final Integer n4 = (int) Math.floor(Math.random() * 9 + 1);
+		final String word = companyName.substring(0, 4).toUpperCase();
+		final String ticker = word + '-' + n1 + n2 + n3 + n4;
+		res = ticker;
+
+		final Collection<Lesson> less = this.lessonRepository.getLessonWithTicker(ticker);
+		if (!less.isEmpty())
+			this.generateTicker(companyName);
 		return res;
 	}
 }
--- Item 3/Educafy/src/main/java/services/TeacherService.java
+++ Item 3/Educafy/src/main/java/services/TeacherService.java
@@ -1,11 +1,217 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.validation.ValidationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+import repositories.CurriculaRepository;
+import repositories.TeacherRepository;
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
+import domain.Actor;
+import domain.Curriculum;
+import domain.Teacher;
+import forms.ActorForm;
 
 @Service
 @Transactional
 public class TeacherService {
+
+	@Autowired
+	private TeacherRepository	teacherRepository;
+
+	@Autowired
+	private ActorService		actorService;
+
+	@Autowired
+	private UserAccountService	userAccountService;
+
+	@Autowired
+	private CurriculaService	curriculaService;
+
+	@Autowired
+	private CurriculaRepository	curriculaRepository;
+
+	@Autowired
+	private Validator			validator;
+
+
+	public Teacher create() {
+		final Teacher teacher = new Teacher();
+		this.actorService.setAuthorityUserAccount(Authority.TEACHER, teacher);
+
+		return teacher;
+	}
+
+	public Teacher findOne(final int teacherId) {
+		Assert.isTrue(teacherId != 0);
+		final Teacher result = this.teacherRepository.findOne(teacherId);
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Teacher save(final Teacher teacher) {
+		Assert.notNull(teacher);
+		Teacher result;
+
+		if (teacher.getId() == 0) {
+			this.actorService.setAuthorityUserAccount(Authority.TEACHER, teacher);
+			result = this.teacherRepository.save(teacher);
+			//			this.folderService.setFoldersByDefault(result);
+
+			final Curriculum curricula = this.curriculaService.createForNewTeacher();
+			curricula.setTeacher(result);
+			final Curriculum res = this.curriculaRepository.save(curricula);
+			Assert.notNull(res);
+
+		} else {
+			this.actorService.checkForSpamWords(teacher);
+			final Actor principal = this.actorService.findByPrincipal();
+			Assert.isTrue(principal.getId() == teacher.getId(), "You only can edit your info");
+			result = (Teacher) this.actorService.save(teacher);
+		}
+		return result;
+	}
+
+	public void delete(final Teacher teacher) {
+		Assert.notNull(teacher);
+		Assert.isTrue(this.findByPrincipal().equals(teacher));
+		Assert.isTrue(teacher.getId() != 0);
+		final Actor principal = this.actorService.findByPrincipal();
+		Assert.isTrue(principal.getId() == teacher.getId(), "You only can edit your info");
+		Assert.isTrue(this.teacherRepository.exists(teacher.getId()));
+		this.teacherRepository.delete(teacher);
+	}
+
+	/* ========================= OTHER METHODS =========================== */
+
+	public Teacher findByPrincipal() {
+		final UserAccount user = LoginService.getPrincipal();
+		Assert.notNull(user);
+
+		final Teacher teacher = this.findByUserId(user.getId());
+		Assert.notNull(teacher);
+		final boolean bool = this.actorService.checkAuthority(teacher, Authority.TEACHER);
+		Assert.isTrue(bool);
+
+		return teacher;
+	}
+
+	public Teacher findByUserId(final int id) {
+		Assert.isTrue(id != 0);
+		final Teacher teacher = this.teacherRepository.findByUserId(id);
+		return teacher;
+	}
+
+	public void flush() {
+		this.teacherRepository.flush();
+	}
+
+	public Teacher reconstruct(final ActorForm actorForm, final BindingResult binding) {
+		Teacher teacher;
+
+		if (actorForm.getId() == 0) {
+			teacher = this.create();
+			teacher.setName(actorForm.getName());
+			teacher.setSurname(actorForm.getSurname());
+			teacher.setPhoto(actorForm.getPhoto());
+			teacher.setPhone(actorForm.getPhone());
+			teacher.setEmail(actorForm.getEmail());
+			teacher.setAddress(actorForm.getAddress());
+			teacher.setVat(actorForm.getVat());
+			teacher.setVersion(actorForm.getVersion());
+			//			student.setScore(0.0);
+			//			student.setSpammer(false);
+			final UserAccount account = this.userAccountService.create();
+			final Collection<Authority> authorities = new ArrayList<>();
+			final Authority auth = new Authority();
+			auth.setAuthority(Authority.STUDENT);
+			authorities.add(auth);
+			account.setAuthorities(authorities);
+			account.setUsername(actorForm.getUserAccountuser());
+			account.setPassword(actorForm.getUserAccountpassword());
+			teacher.setUserAccount(account);
+		} else {
+			teacher = this.teacherRepository.findOne(actorForm.getId());
+			teacher.setName(actorForm.getName());
+			teacher.setSurname(actorForm.getSurname());
+			teacher.setPhoto(actorForm.getPhoto());
+			teacher.setPhone(actorForm.getPhone());
+			teacher.setEmail(actorForm.getEmail());
+			teacher.setAddress(actorForm.getAddress());
+			teacher.setVat(actorForm.getVat());
+			teacher.setVersion(actorForm.getVersion());
+			final UserAccount account = this.userAccountService.findOne(teacher.getUserAccount().getId());
+			account.setUsername(actorForm.getUserAccountuser());
+			account.setPassword(actorForm.getUserAccountpassword());
+			teacher.setUserAccount(account);
+		}
+
+		this.validator.validate(teacher, binding);
+		if (binding.hasErrors())
+			throw new ValidationException();
+
+		return teacher;
+	}
+
+	public Teacher findTeacherByCurricula(final int curriculaId) {
+		final Teacher result = this.teacherRepository.findTeacherByCurricula(curriculaId);
+		return result;
+	}
+
+	public Teacher findTeacherByPersonalRecord(final int personalRecorId) {
+		final Teacher result = this.teacherRepository.findTeacherByPersonalRecord(personalRecorId);
+		return result;
+	}
+
+	public Teacher findTeacherByMiscellaneous(final int miscellaneousRecordId) {
+		final Teacher result = this.teacherRepository.findTeacherByMiscellaneous(miscellaneousRecordId);
+		return result;
+	}
+
+	public Teacher findTeacherByEducationRecords(final int educationRecordId) {
+		final Teacher result = this.teacherRepository.findTeacherByEducationRecords(educationRecordId);
+		return result;
+	}
+
+	public Boolean hasPersonalRecord(final int teacherId, final int personalRecordId) {
+		final Boolean result = this.teacherRepository.hasPersonalRecord(teacherId, personalRecordId);
+		Assert.notNull(result, "hasPersonalData returns null");
+		return result;
+	}
+
+	public Boolean hasEducationRecord(final int teacherId, final int educationRecordId) {
+		final Boolean result = this.teacherRepository.hasEducationRecord(teacherId, educationRecordId);
+		Assert.notNull(result, "hasEducationData returns null");
+		return result;
+	}
+
+	public Boolean hasMiscellaneousRecord(final int teacherId, final int dataId) {
+		final Boolean result = this.teacherRepository.hasMiscellaneousRecord(teacherId, dataId);
+		Assert.notNull(result, "hasMiscellanousData returns null");
+		return result;
+	}
+
+	public Boolean hasCurricula(final int teacherId, final int miscellaneousRecordId) {
+		final Boolean result = this.teacherRepository.hasCurricula(teacherId, miscellaneousRecordId);
+		Assert.notNull(result, "hasCurricula returns null");
+		return result;
+	}
+
+	//	public Teacher findTeacherByCopyCurricula(final int curriculaId) {
+	//		final Teacher result = this.teacherRepository.findTeacherByCopyCurricula(curriculaId);
+	//		Assert.notNull(result, "teacher found by copy of curricula is null");
+	//		return result;
+	//	}
 
 }
